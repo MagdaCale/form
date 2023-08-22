39dcd519e394f40a21352de2bfe51bd348939c38
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2023-08-22 11:19:13 +0200

    first commit

---
+++ asstes/main.js
@@ -0,0 +1,9 @@
+console.log("Hallo Welt!");
+
+
+function feeling() {
+    let range = document.querySelector("input[type='range']").value;
+    let output = document.querySelector(".output")
+    output.innerHTML= `Ich fühle mich wie eine ${range}`
+}
+
